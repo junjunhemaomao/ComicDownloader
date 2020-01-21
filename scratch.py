ad67c5a256a9ebd13d3ceece7225daaefd58241a
junjunhemaomao <502581939@qq.com>
2020-01-22 02:53:00 +0800

    Delete scratch.py

--- scratch.py
+++
@@ -1,8 +0,0 @@
-import requests
-import re
-from bs4 import BeautifulSoup
-from lxml import etree
-
-response = requests.get('http://www.mangabz.com/m10344/').content.decode()
-img = re.findall('(i?)<img.*? src=\"?(.*?\\.(jpg|gif|bmp|bnp|png))\".*? />',response)
-print(img)
