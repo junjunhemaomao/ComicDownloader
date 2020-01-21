56c723dc0f86fb31906451c0e9eb962671ad0525
junjun <502581939@qq.com>
2020-01-22 04:05:58 +0800

    获取元素，操作事件
    研究获取元素
    操作事件

--- downloader.py
+++ downloader.py
@@ -1,6 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import keys
+from selenium.webdriver.common.action_chains import
+import time
+import random
 
 br = webdriver.Chrome()
-br.get("http://www.mangabz.com/m10344/")
-
-
+url = br.get("http://www.mangabz.com/m10344/")
+wait_time = random(0.1,1)
+time.sleep(wait_time)
+image = url.find_element_by_xpath("//*[(@id = "cp_image")]")
