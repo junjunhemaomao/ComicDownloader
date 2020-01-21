69f38dddb40b189bd207eafbac789f4743f5941f
junjun <502581939@qq.com>
2020-01-22 02:51:08 +0800

    get first
    get first

---
+++ analyseData.py
@@ -0,0 +1,27 @@
+
+#网页图片地址。自己的命名是无序的，需要添加一个重命名函数。
+
+http://image.mangabz.com/1/73/10344/1_8453.jpg?cid=10344&key=20a535c46ca6d5622acd90cca1503fc3&uk=
+
+#网页解析出来的请求头
+
+GET /1/73/10344/1_8453.jpg?cid=10344&key=20a535c46ca6d5622acd90cca1503fc3&uk= HTTP/1.1
+Referer: http://www.mangabz.com/m10344/
+User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36
+
+#网页解析出来的返回头
+
+HTTP/1.1 200 OK
+Date: Tue, 21 Jan 2020 17:34:26 GMT
+Content-Type: image/jpeg
+Content-Length: 171613
+Last-Modified: Fri, 05 Jul 2019 02:36:40 GMT
+ETag: "5d1eb7b8-29e5d"
+Expires: Thu, 31 Dec 2037 23:55:55 GMT
+Cache-Control: max-age=315360000
+CF-Cache-Status: MISS
+Accept-Ranges: bytes
+Vary: Accept-Encoding
+Server: cloudflare
+CF-RAY: 558b09f9b9b6eaec-LAX
+
---
+++ downloader.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+
+br = webdriver.Chrome()
+br.get("http://www.mangabz.com/m10344/")
+
+
