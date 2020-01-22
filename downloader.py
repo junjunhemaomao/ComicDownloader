13d411d1d122360201f12153f098357425eca47f
junjun <502581939@qq.com>
2020-01-22 14:34:38 +0800

    模拟鼠标事件点击
    获取图片信息还有知识盲点，还不能分析出来。
    鼠标事件还不透彻。
    批处理程序暂时还没顾上

--- downloader.py
+++ downloader.py
@@ -1,11 +1,25 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import keys
-from selenium.webdriver.common.action_chains import
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+import pyautogui
+from time import sleep
+import requests
 import time
 import random
 
-br = webdriver.Chrome()
-url = br.get("http://www.mangabz.com/m10344/")
-wait_time = random(0.1,1)
-time.sleep(wait_time)
-image = url.find_element_by_xpath("//*[(@id = "cp_image")]")
+
+web = webdriver.Chrome()
+web.get("http://www.mangabz.com/m10344/")
+wait = WebDriverWait(web,10)
+#wait_time = random(0.1,1)
+#time.sleep(wait_time)
+img = wait.until(EC.element_to_be_clickable((By.TAG_NAME,'img')))
+actions = ActionChains(web)
+actions.context_click(img)
+actions.perform()
+pyautogui.typewrite(['down','down','down','down','down','down','down','enter','enter'])
+sleep(0.2)
+pyautogui.typewrite(['enter'])
